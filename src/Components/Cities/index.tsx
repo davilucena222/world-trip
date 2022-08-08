cd34a92bc4bba2eb931d9e659341626d9c761d13
Davi Luiz <davilucena222@gmail.com>
2022-08-08 12:54:07 +0000

    cities components

--- src/Components/Cities/index.tsx
+++ src/Components/Cities/index.tsx
@@ -1,8 +1,9 @@
 import { Grid, Heading } from "@chakra-ui/react";
+import { City } from "./City";
 
 export function Cities() {
-  return(
-    <>
+  return (
+    <div>
       <Heading
         fontWeight="500"
         fontSize={["2xl", "4xl"]}
@@ -10,6 +11,19 @@ export function Cities() {
       >
         Cidades +100
       </Heading>
-    </>
+      <Grid
+        templateColumns={["1fr", "1fr 1fr", "repeat(3, 1fr)", "repeat(4, 1fr)"]}
+        gap={["20px", "45px"]}
+        alignItems="center"
+        justifyContent="center"
+        px={["30px", "0"]}
+      >
+        <City />
+        <City />
+        <City />
+        <City />
+        <City />
+      </Grid>
+    </div>
   );
 }
