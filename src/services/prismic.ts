9272c5b33064a17953921b5633951b4f8a1d2910
Davi Luiz <davilucena222@gmail.com>
2022-08-09 11:09:21 -0300

    changing prismic settings

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,7 +2,7 @@ import * as Prismic from '@prismicio/client'
 
 export function getPrismicClient() {
   const prismic = Prismic.createClient(
-    process.env.PRISMIC_ENDPOINT,
+    process.env.PRISMIC_API_ENDPOINT,
     {
       accessToken: process.env.PRISMIC_ACCESS_TOKEN
     }
